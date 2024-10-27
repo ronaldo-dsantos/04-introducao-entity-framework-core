2711691c52a36252f4b4487ef52879acef38cb15
ronaldo <ronaldo.dsantos@gmail.com>
2024-10-26 23:10:53 -0300

    add migrations

--- CursoEFCore/Program.cs
+++ CursoEFCore/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace CursoEFCore;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoEFCore;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    using var db = new Data.ApplicationContext();
+    //db.Database.Migrate();
+
+    var migracaoPendente = db.Database.GetPendingMigrations().Any(); // GetPendingMigrations() para verificar se há migrações pendentes | Any() para transformar em booleano 
+
+    if (migracaoPendente)
     {
-        Console.WriteLine("Hello, World!");
+      // refra de negócio se houver migracao pendente
     }
+  }
 }
